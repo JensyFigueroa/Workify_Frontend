2ccdcc9a4f9cdabe40d4fa10f42682e5c6342258
Jensy Figueroa <jensy1418@gmail.com>
2023-05-16 20:02:48 -0400

    Navbar y SearchBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,53 +1,72 @@
-import React from "react";
+import { NavLink } from 'react-router-dom'
+import styles from './Navbar.module.css'
+import { useEffect, useState } from 'react'
+import SearchBar from '../searchBar/SearchBar';
+
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const [clickBurguer, setClickBurguer] = useState(false);
+
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+  });
+
+  if (screenSize.width > 1023 & clickBurguer) {
+    setClickBurguer(false)
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+      });
+    };
+    window.addEventListener('resize', handleResize);
+  }, []);
+
+  const handleClick = () => {
+    setClickBurguer(!clickBurguer)
+  }
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo03"
-          aria-controls="navbarTogglerDemo03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+    <>
+      <nav className={styles.container}>
+        <div>
+          <h1>Logo</h1>
         </div>
-      </div>
-    </nav>
+
+        <div>
+          <SearchBar />
+        </div>
+
+        <div className={`${styles.links} ${clickBurguer ? styles.show : ''} `}>
+
+          <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : styles.link)} onClick={handleClick}>About</NavLink>
+
+        </div>
+
+        <div className="btn-group btn" role="group" >
+          <button type="button" className={`${styles.btn} btn btn-primary dropdown-toggle`} style={{color:'black'}} data-bs-toggle="dropdown" aria-expanded="false">
+            <BsFillPersonLinesFill style={{color:'black'}}/>
+          </button>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Login</a></li>
+            <li><a className="dropdown-item" href="#">Log Out</a></li>
+          </ul>
+        </div>
+
+        <div className={`${styles.btnBurguer} ${styles.navIcon} ${clickBurguer ? styles.open : ''}`} onClick={handleClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div className={`${styles.curtain} ${clickBurguer ? styles.showCurtain : ''}`}></div>
+
+      </nav>
+    </>
+
+
   );
 };
 
