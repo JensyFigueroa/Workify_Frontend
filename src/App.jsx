ad3753f4a820e70e21ee2ce9ea2c53089c707602
Luishaedo <luismariahaedolanda@gmail.com>
2023-05-17 20:59:38 -0300

    filterBar & app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { CreateService } from "./components/views/CreateService/CreateService";
 import { Detail } from "./components/views/Detail/Detail";
 import Home from "./components/views/Home/Home";
 import Navbar from "./components/navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./components/views/landing/Landing";
+import FilterBar from "./components/FilterBar/filterBar";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <Navbar />
+      <FilterBar />
       <Home />
 
       <Routes>
-        <Route exact path="/" element={<Landing/>}></Route>
+        <Route exact path="/" element={<Landing />}></Route>
         <Route path="/detail/:id" element={<Detail />}></Route>
         <Route path="/createService" element={<CreateService />}></Route>
       </Routes>
