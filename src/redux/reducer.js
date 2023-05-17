2907d75ed9b0f1b7dfb1ef8584a9574c85414dae
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-16 22:55:41 -0300

    2 endpoint creados en las actionsCreator

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,18 +1,29 @@
-import { GET_SERVICES } from "./types";
+import { GET_SERVICES, GET_SERVICESBYNAME, GET_SERVICESDETAIL } from "./types";
 
 const initialState = {
   allServices: [],
+  serviceDetail: {},
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-      case GET_SERVICES:
-        return {
-          ...state, 
-          allServices: action.payload
-        }
-        default:
-          return {...state};
+    case GET_SERVICES:
+      return {
+        ...state,
+        allServices: action.payload,
+      };
+    case GET_SERVICESBYNAME:
+      return {
+        ...state,
+        allServices: action.payload,
+      };
+    case GET_SERVICESDETAIL:
+      return {
+        ...state,
+        serviceDetail: action.payload,
+      };
+    default:
+      return { ...state };
   }
 };
 
