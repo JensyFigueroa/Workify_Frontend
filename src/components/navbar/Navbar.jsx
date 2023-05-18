af014877aaf33e02d74590d4e9f7bad9d3940486
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-17 23:24:51 -0300

    listo que el navbar no aparezca en landing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,21 @@
-import { NavLink } from 'react-router-dom'
-import styles from './Navbar.module.css'
-import { useEffect, useState } from 'react'
-import SearchBar from '../searchBar/SearchBar';
-
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import { useEffect, useState } from "react";
+import SearchBar from "../searchBar/SearchBar";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { MdHomeRepairService } from "react-icons/md";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  let location = useLocation();
   const [clickBurguer, setClickBurguer] = useState(false);
 
   const [screenSize, setScreenSize] = useState({
     width: window.innerWidth,
   });
 
-  if (screenSize.width > 1023 & clickBurguer) {
-    setClickBurguer(false)
+  if ((screenSize.width > 1023) & clickBurguer) {
+    setClickBurguer(false);
   }
 
   useEffect(() => {
@@ -23,53 +24,93 @@ const Navbar = () => {
         width: window.innerWidth,
       });
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
   }, []);
 
   const handleClick = () => {
-    setClickBurguer(!clickBurguer)
-  }
+    setClickBurguer(!clickBurguer);
+  };
   return (
-    <>
-      <nav className={styles.container}>
-        <div className={styles.logo}>
-          <MdHomeRepairService style={{fontSize:'63px'}}></MdHomeRepairService>
-          <h1>Workify</h1>
-        </div>
+    location.pathname !== "/" && (
+      <>
+        <nav className={styles.container}>
+          <div className={styles.logo}>
+            <MdHomeRepairService
+              style={{ fontSize: "63px" }}
+            ></MdHomeRepairService>
+            <h1>Workify</h1>
+          </div>
 
-        <div className={styles.search}>
-          <SearchBar />
-        </div>
+          <div className={styles.search}>
+            <SearchBar />
+          </div>
 
-        <div className={`${styles.links} ${clickBurguer ? styles.show : ''} `}>
+          <div
+            className={`${styles.links} ${clickBurguer ? styles.show : ""} `}
+          >
+            <NavLink
+              to="/home"
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              onClick={handleClick}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? styles.active : styles.link
+              }
+              onClick={handleClick}
+            >
+              About
+            </NavLink>
+          </div>
 
-          <NavLink to='/home' className={({ isActive }) => (isActive ? styles.active : styles.link)} onClick={handleClick}>Home</NavLink>
-          <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : styles.link)} onClick={handleClick}>About</NavLink>
+          <div className="btn-group btn" role="group">
+            <button
+              type="button"
+              className={`${styles.btn} btn btn-primary dropdown-toggle`}
+              style={{ color: "black" }}
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <BsFillPersonLinesFill style={{ color: "black" }} />
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Login
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Log Out
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        </div>
+          <div
+            className={`${styles.btnBurguer} ${styles.navIcon} ${
+              clickBurguer ? styles.open : ""
+            }`}
+            onClick={handleClick}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
 
-        <div className="btn-group btn" role="group" >
-          <button type="button" className={`${styles.btn} btn btn-primary dropdown-toggle`} style={{color:'black'}} data-bs-toggle="dropdown" aria-expanded="false">
-            <BsFillPersonLinesFill style={{color:'black'}}/>
-          </button>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Login</a></li>
-            <li><a className="dropdown-item" href="#">Log Out</a></li>
-          </ul>
-        </div>
-
-        <div className={`${styles.btnBurguer} ${styles.navIcon} ${clickBurguer ? styles.open : ''}`} onClick={handleClick}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-
-        <div className={`${styles.curtain} ${clickBurguer ? styles.showCurtain : ''}`}></div>
-
-      </nav>
-    </>
-
-
+          <div
+            className={`${styles.curtain} ${
+              clickBurguer ? styles.showCurtain : ""
+            }`}
+          ></div>
+        </nav>
+      </>
+    )
   );
 };
 
