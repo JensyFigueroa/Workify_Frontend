180c3ec7f942b4c16a31c7689d153c8360e26d05
Luishaedo <luismariahaedolanda@gmail.com>
2023-05-17 23:09:59 -0300

    solucion conflicto

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,14 @@ import Home from "./components/views/Home/Home";
 import Navbar from "./components/navbar/Navbar";
 import LandingPage from "./components/views/landing/Landing";
 import { Route, Routes } from "react-router-dom";
-import Landing from "./components/views/landing/Landing";
-import FilterBar from "./components/FilterBar/filterBar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <FilterBar />
-      <Home />
       <Routes>
         <Route path={"/"} element={<LandingPage />} />
-        <Route exact path="/" element={<Landing />}></Route>
+        <Route path={"/home"} element={<Home />} />
         <Route path="/detail/:id" element={<Detail />}></Route>
         <Route path="/createService" element={<CreateService />}></Route>
       </Routes>
