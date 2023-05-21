7308723a6aaec01c8814ba74a347e5128d4752f5
Jensy Figueroa <jensy1418@gmail.com>
2023-05-21 18:44:27 -0400

    Se le quito los estilos al footer

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import styles from './SearchBar.module.css'
 
 const SearchBar = () => {
+
+  const [name, setName] = useState()
 
     return (
         <>
             <form className={styles.search}>
                 <div className="input-group">
-                    <input type="text" className={`${styles.inputSearch} form-control`} placeholder="Search service or worker" aria-label="Server" />
+                    <input type="text" className={`${styles.inputSearch} form-control`} placeholder="Search service" aria-label="Server" />
                     <span className={`${styles.inputGroupText} input-group-text`}><button className={styles.location}><i className="fa-solid fa-magnifying-glass"></i></button></span>
                 </div>
             </form>
