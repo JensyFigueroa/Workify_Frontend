33dc8d4244b0cd47290310a46d71cb8c9c0976d5
Leguiza Gonzalo <leguizagonzalo@gmail.com>
2023-05-24 15:36:19 -0300

    Diseño de footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/views/Home/Home";
 import Navbar from "./components/navbar/Navbar";  
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./components/views/landing/LandingPage";
-import Footer from "./components/views/footer/footer";
+import Footer from "./components/views/footer/Footer";
 
 function App() {
   return (
@@ -16,7 +16,8 @@ function App() {
         <Route path="/detail/:id" element={<Detail />}></Route>
         <Route path="/createService" element={<CreateService />}></Route>
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
+
     </>
   );
 }
