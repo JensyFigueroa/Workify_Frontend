a177ecc0e69135e1d5f88b9eaf7d189e97ccd949
Jensy Figueroa <jensy1418@gmail.com>
2023-05-26 16:05:54 -0400

    create form login-user

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,28 +7,42 @@ import { useState } from 'react';
 import validate from './validate'
 
 const Login = () => {
-
-    const [formLogin, setFormUser] = useState({ email: '', password: ''});
+    const [formLogin, setFormLogin] = useState(null);
+    const [formUser, setFormUser] = useState(null);
     const [errors, setErrors] = useState({});
-    
-    const handleInputChange = (e) => {
-        console.log('handleInputChange ',e.target.value);
+
+    const handleInputChangeLogin = (e) => {
         const { name, value } = e.target
-        
-        setFormUser({...formLogin, [name]:value})
-    } 
-    
+        setFormLogin({ ...formLogin, [name]: value })
+        setFormUser(null)
+    }
+
+    const handleInputChangeUser = (e) => {
+        const { name, value } = e.target
+        setFormUser({ ...formUser, [name]: value })
+        setFormLogin(null)
+    }
+
     const handleBlur = (e) => {
-        handleInputChange(e);
-        setErrors(validate(formLogin))
+        handleInputChangeLogin(e);
+        console.log(formLogin,formUser)
+        console.log(formLogin)
+        if (formLogin) setErrors(validate(formLogin));
+        if (formUser) setErrors(validate(formUser));
         console.log('estoy en el blur')
-      }
-    
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Enviar el form ', formLogin);
-    
+        console.log('Enviar el form Login ', formLogin);
     }
+
+    const handleSubmitUser = (e) => {
+        e.preventDefault();
+        console.log('Enviar el form User', formUser);
+    }
+
+
     return (
         <div className="btn-group " role="group">
             <button
@@ -52,12 +66,10 @@ const Login = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link className="dropdown-item" to="#" style={{ color: 'blue' }} data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">
+                    <Link className="dropdown-item" to="#" style={{ color: 'blue' }} data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" >
                         Create User
                     </Link>
-                    <Link className="dropdown-item" to="#" style={{ color: 'blue' }} data-bs-target="#exampleModalToggle3" data-bs-toggle="modal">
-                        Create Lender
-                    </Link>
+            
                 </li>
             </ul>
 
@@ -68,21 +80,21 @@ const Login = () => {
                     <div className={`${styles.wrapper} modal-content`}>
                         <div className={`${styles.titleLogin} modal-header`}>
                             <h1 className="modal-title fs-5" id="exampleModalToggleLabel">Login Workify</h1>
-                            <button type="button" className="btn" style={{ color: 'white', fontWeight: '600', fontSize: '30px' }} data-bs-dismiss="modal">X</button>
+                            <button type="button" className="btn" style={{ color: 'white', fontWeight: '600', fontSize: '30px' }} data-bs-dismiss="modal" name='btnLogin' >X</button>
                         </div>
                         <div className="modal-body">
                             <div className="mb-6">
                                 <form onSubmit={handleSubmit}>
                                     <div className={styles.field}>
-                                        <input type="text" name='email' onChange={handleInputChange} onBlur={handleBlur} required />
+                                        <input type="text" name='email' onChange={handleInputChangeLogin} onBlur={handleBlur} required />
                                         <label htmlFor="">Email address</label>
                                     </div>
-                                        {errors.email && <p style={{ color: 'red', fontStyle: 'italic', fontSize: '18px'}}>{errors.email}</p>}
+                                    {errors.email && <p style={{ color: 'red', fontStyle: 'italic', fontSize: '18px' }}>{errors.email}</p>}
                                     <div className={styles.field}>
-                                        <input type="password" name='password' onChange={handleInputChange} onBlur={handleBlur} required/>
+                                        <input type="password" name='passwordLogin' onChange={handleInputChangeLogin} onBlur={handleBlur} required />
                                         <label htmlFor="">Password</label>
                                     </div>
-                                        {errors.password && <p style={{ color: 'red', fontStyle: 'italic', fontSize: '18px' }}>{errors.password}</p>}
+                                    {errors.password && <p style={{ color: 'red', fontStyle: 'italic', fontSize: '18px' }}>{errors.password}</p>}
 
                                     <div className={styles.content}>
                                         <div className={styles.checkbox}>
@@ -102,9 +114,7 @@ const Login = () => {
                                     <div className={styles.signUpLink}>
                                         Don`t have an account?
                                         <div className={styles.typeAccount}>
-                                            <Link to={'#'} data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">SignUp User</Link>
-                                            <Link to={'#'} data-bs-target="#exampleModalToggle3" data-bs-toggle="modal">SignUp Lender</Link>
-
+                                            <Link to={'#'} data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" >SignUp User</Link>
                                         </div>
                                     </div>
                                 </form>
@@ -112,9 +122,9 @@ const Login = () => {
                         </div>
 
                         <div className="modal-footer">
-                        
-                                <button type='submit' className={styles.btnGoogle}><FcGoogle className={styles.icoGoogle} /> Continue with Google</button>
-                        
+
+                            <button type='submit' className={styles.btnGoogle}><FcGoogle className={styles.icoGoogle} /> Continue with Google</button>
+
                         </div>
                     </div>
                 </div>
@@ -124,36 +134,36 @@ const Login = () => {
                     <div className={`${styles.wrapper} modal-content`}>
                         <div className={`${styles.titleLogin} modal-header`}>
                             <h1 className="modal-title fs-5" id="exampleModalToggleLabel">Create User</h1>
-                            <button type="button" className="btn" style={{ color: 'white', fontWeight: '600', fontSize: '30px' }} data-bs-dismiss="modal">X</button>
+                            <button type="button" className="btn" style={{ color: 'white', fontWeight: '600', fontSize: '30px' }} data-bs-dismiss="modal" name='btnUser'>X</button>
                         </div>
                         <div className="modal-body">
-                            <form action="">
+                            <form onSubmit={handleSubmitUser}>
                                 <div className={styles.field}>
-                                    <input type="text" required />
+                                    <input type="text" name='firstName' onChange={handleInputChangeUser} onBlur={handleBlur} required />
                                     <label htmlFor="">Firstname </label>
                                 </div>
                                 <div className={styles.field}>
-                                    <input type="text" required />
+                                    <input type="text" name='lastName' onChange={handleInputChangeUser} onBlur={handleBlur} required />
                                     <label htmlFor="">Lastname</label>
                                 </div>
                                 <div className={styles.field}>
-                                    <input type="text" required />
+                                    <input type="text" name='phoneNumber' onChange={handleInputChangeUser} onBlur={handleBlur} required />
                                     <label htmlFor="">Phone number</label>
                                 </div>
                                 <div className={styles.field}>
-                                    <input type="text" required />
+                                    <input type="text" name='country' onChange={handleInputChangeUser} onBlur={handleBlur} required />
                                     <label htmlFor="">Country</label>
                                 </div>
                                 <div className={styles.field}>
-                                    <input type="text" required />
+                                    <input type="text" name='city' onChange={handleInputChangeUser} onBlur={handleBlur} required />
                                     <label htmlFor="">City</label>
                                 </div>
                                 <div className={styles.field}>
-                                    <input type="password" required />
+                                    <input type="password" name='passwordUser' onChange={handleInputChangeUser} onBlur={handleBlur} required />
                                     <label htmlFor="">Password</label>
                                 </div>
                                 <div className={styles.field}>
-                                    <input type="password" required />
+                                    <input type="passwordConfirmUser" required />
                                     <label htmlFor="">Confirm Password</label>
                                 </div>
                                 <button type="submit" className={`btn btn-primary ${styles.field}`}>Create User</button>
@@ -169,7 +179,7 @@ const Login = () => {
                     <div className={`${styles.wrapper} modal-content`}>
                         <div className={`${styles.titleLogin} modal-header`}>
                             <h1 className="modal-title fs-5" id="exampleModalToggleLabel">Create Lender</h1>
-                            <button type="button" className="btn" style={{ color: 'white', fontWeight: '600', fontSize: '30px' }} data-bs-dismiss="modal">X</button>
+                            <button type="button" className="btn" style={{ color: 'white', fontWeight: '600', fontSize: '30px' }} data-bs-dismiss="modal" name='btnLender' >X</button>
                         </div>
 
                         <div className="modal-body">
@@ -208,11 +218,11 @@ const Login = () => {
                                         <label htmlFor="">Contact phone number</label>
                                     </div>
                                     <div className={styles.fieldC}>
-                                        <input type="password" required />
+                                        <input type="passwordLender" required />
                                         <label htmlFor="">Password</label>
                                     </div>
                                     <div className={styles.fieldC}>
-                                        <input type="password" required />
+                                        <input type="passwordConfirmLender" required />
                                         <label htmlFor="">Confirm Password</label>
                                     </div>
                                 </div>
