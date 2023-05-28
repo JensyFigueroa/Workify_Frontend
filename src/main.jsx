ccf908be826a2afba20857d31d5d85dea95febc3
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-28 14:25:48 -0300

    notificacion cuando se agrega productos al carrito listas

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,24 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          success: {
+            style: {},
+          },
+          error: {
+            style: {},
+          },
+          style: {},
+        }}
+      />
     </BrowserRouter>
   </Provider>
 );
