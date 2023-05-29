36f26154b36d1a87202b59beada2b6d5242b41cc
Danieltm95 <docampoc95@gmail.com>
2023-05-28 19:22:00 -0500

    agregue a la whilites de redux  persistance la variable currentUserNameLoggedIn

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import thunk from "redux-thunk";
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['currentUserIdLoggedIn'],
+  whitelist: ['currentUserIdLoggedIn','currentUserNameLoggedIn'],
 };
 
 const middleware = [thunk];
