e11dc3da48389e8807f8be38375c1bd8c916a65d
Carlitossaul <carlitossaul.h@gmail.com>
2023-05-31 17:20:22 -0300

    creado componente consent

---
+++ src/components/Consent/Consent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Consent.module.css";
+
+const Consent = () => {
+  return (
+    <div>
+      <h1>Contratos</h1>
+    </div>
+  );
+};
+
+export default Consent;
