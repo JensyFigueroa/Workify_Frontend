8f87bc399ec1cffa7fa753f234b8372cbe29666e
Joaquingro <joaquinsgro@gmail.com>
2023-06-01 01:31:10 -0600

    Carrusel

--- src/components/views/Detail/Detail.jsx
+++ src/components/views/Detail/Detail.jsx
@@ -3,106 +3,53 @@ import { getServiceDetail, cleanDetail, addServiceInCart } from "../../../redux/
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import style from "./Detail.module.css";
-import toast, { Toaster } from "react-hot-toast";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import  Carousel  from "./Carousel/Carousel";
+
 
 export function Detail() {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const notify = () =>
-    toast.success("Your reservation has been successfully completed.");
-
+  const [comments, setComments] = useState([])
+  const [inputs, setInputs] = useState({
+    name: "",
+    comment: "",
+  })
+  const currentUserNameLoggedIn = useSelector(state => state.currentUserNameLoggedIn);
+  const serviceDetail = useSelector((state) => state.serviceDetail);
+  let arrImage = Array.isArray(serviceDetail.imageUrl) ? [...serviceDetail.imageUrl] : [serviceDetail.imageUrl];
+  const { nameService, pricePerHour } = serviceDetail;
+  
   useEffect(() => {
     dispatch(getServiceDetail(id));
     return () => {
       dispatch(cleanDetail());
     };
   }, [dispatch, id]);
-  const currentUserNameLoggedIn = useSelector(state => state.currentUserNameLoggedIn);
-  const serviceDetail = useSelector((state) => state.serviceDetail);
-  console.log(serviceDetail);
-  let arrImage = [serviceDetail.imageUrl];
 
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
-  const [isDateTimeSelected, setIsDateTimeSelected] = useState(false);
-  const [startTime, setStartTime] = useState(null);
-  const [endTime, setEndTime] = useState(null);
-  const [hours, setHours] = useState(0);
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
-  const handleStartTimeChange = (time) => {
-    setStartTime(time);
-  };
-
-  const handleEndTimeChange = (time) => {
-    setEndTime(time);
-    const start = new Date(startTime);
-    const end = new Date(time);
-    const timeDiff = end - start;
-    const hoursDiff = Math.round(timeDiff / (1000 * 60 * 60));
-    setHours(hoursDiff);
-    setIsDateTimeSelected(true);
-  };
-
-  useEffect(() => {
-    setIsButtonEnabled(isDateTimeSelected);
-  }, [isDateTimeSelected]);
-
-  const handleReservation = () => {
-    const quantity = hours;
-    const date = new Date(selectedDate);
-    //fecha y hora de inicio reserva
-    const start = new Date(selectedDate);
-    start.setHours(startTime.getHours());
-    start.setMinutes(startTime.getMinutes());
-    //fecha y hora de finalizacion de reserav
-    const end = new Date(selectedDate);
-    end.setHours(endTime.getHours());
-    end.setMinutes(endTime.getMinutes());
-    //obj para enviar al carrito(x redux)
-    const reservation = {
-      idService: serviceDetail.id,
-      nameService: serviceDetail.nameService,
-      typeService: serviceDetail.typeService,
-      location: serviceDetail.location,
-      description: serviceDetail.description,
-      pricePerHour: serviceDetail.pricePerHour || null,
-      reservationData: {
-        date,
-        start,
-        end,
-        quantity,
-      },
-    };
-
-    console.log("Reservation", reservation);
-    console.log("Start Time:", start);
-    console.log("End Time:", end);
-    console.log("Hours:", hours);
-    console.log("queantity", quantity);
-    // notify();
-  };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    })
+  } 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+  
 
   return (
     <div className={style.containerFather}>
     <div className={style.detailContainer}>
 
        <div className={style.featuredProjectsContainer}>
-          {/* <h2 className={style.featuredProjectsTitle}>Featured Projects</h2> */}
-          {arrImage.length > 0 &&
-            arrImage.map((image, index) => (
-              <img
-                className={style.featuredProjectImage}
-                src={image}
-                alt={`Featured Project ${index + 1}`}
-                key={index}
-              />
-            ))}
+          {Array.isArray(serviceDetail.imageUrl) &&
+          arrImage.length > 0 && <Carousel images={arrImage} />}
         </div>
 
       <div className={style.serviceInfoContainer}>
@@ -135,89 +82,191 @@ export function Detail() {
         </div>
         <button className={`${style.myButton} btn btn-outline-secondary`}
             onClick={() =>
-              dispatch(addServiceInCart(serviceDetail.id, serviceDetail.nameService, serviceDetail.pricePerHour))
+              dispatch(addServiceInCart(id, nameService, pricePerHour))
             }
           >
             Add to cart
           </button>
       </div>
-      
-      {/* <div className={style.reservationContainer}>
-        <div className={style.calendarContainer}>
-          <p className={style.selectDateLabel}>Select Date:</p>
-          <DatePicker
-            selected={selectedDate}
-            onChange={handleDateChange}
-            minDate={new Date()}
-            dateFormat="dd/MM/yyyy"
-            className={style.datePicker}
-            readOnlyInput
-          />
-        </div>
-        <div className={style.calendarContainer}>
-          <p className={style.selectTimeLabel}>Select Start Time:</p>
-          <DatePicker
-            selected={startTime}
-            onChange={handleStartTimeChange}
-            showTimeSelect
-            showTimeSelectOnly
-            timeIntervals={60}
-            timeCaption="Time"
-            dateFormat="HH:mm"
-            className={style.datePicker}
-            disabled={!selectedDate}
-            readOnlyInput
-          />
-        </div>
-        <div className={style.calendarContainer}>
-          <p className={style.selectTimeLabel}>Select End Time:</p>
-          <DatePicker
-            selected={endTime}
-            onChange={handleEndTimeChange}
-            showTimeSelect
-            showTimeSelectOnly
-            timeIntervals={60}
-            timeCaption="Time"
-            dateFormat="HH:mm"
-            className={style.datePicker}
-            disabled={!startTime}
-            minTime={
-              startTime
-                ? new Date(startTime.getTime() + 60 * 60 * 1000)
-                : new Date().setHours(23, 0)
-            }
-            maxTime={new Date().setHours(23, 0)}
-            readOnlyInput
-          />
-        </div>
-        <p>Hours Selected: {hours}</p>
-        <button
-          onClick={handleReservation}
-          className={`${style.reservationButton} ${
-            isButtonEnabled ? style.enabled : style.disabled
-          }`}
-          disabled={!isButtonEnabled}
-        >
-          Confirm Reservation
-        </button>
-      </div>
-      <Toaster
-        position="bottom-right"
-        toastOptions={{
-          className: "",
-          style: {
-            border: "1px solid #713200",
-            padding: "16px",
-            color: "#713200",
-          },
-        }}
-      /> */}
       </div>
       <div className={style.serviceReviewsContainer}>
           <h1 className={style.serviceReviewsTitle}>Reviews</h1>
           <p>Customers rated this pro highly for work quality, professionalism, and responsiveness.</p>
           <p className={style.serviceReviews}>{serviceDetail.reviews}</p>
         </div>
+        <div>
+
+    <div className={style.form}>
+      <h3>Comments</h3>
+      <ul>
+        {comments.map((com, index) => (
+          <li key={index}>{com}</li>
+        ))}
+      </ul>
+        <form className={style.inputs} onSubmit={handleSubmit}>
+              <input 
+                placeholder="Name"
+                value={inputs.name}
+                onChange={handleInputChange}
+                type="name"
+                className={`form-control`}
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                required
+              />
+              <textarea
+                id="validationTextarea"
+                className={`form-control`}
+                placeholder="Comment"
+                value={inputs.comment}
+                onChange={handleInputChange}
+                required
+              ></textarea>
+              <button className={`${style.myButton} btn btn-outline-secondary`} type="submit">Send comment</button>
+        </form>
+          
+      </div>
+    </div>
     </div>
   );
-}
+
+  }
+ //<---CODIGO DE LUIS--->
+//  import toast, { Toaster } from "react-hot-toast";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+// const [selectedDate, setSelectedDate] = useState(null);
+  // const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+  // const [isDateTimeSelected, setIsDateTimeSelected] = useState(false);
+  // const [startTime, setStartTime] = useState(null);
+  // const [endTime, setEndTime] = useState(null);
+  // const [hours, setHours] = useState(0);
+
+  // const handleDateChange = (date) => {
+  //   setSelectedDate(date);
+  // };
+
+  // const handleStartTimeChange = (time) => {
+  //   setStartTime(time);
+  // };
+
+  // const handleEndTimeChange = (time) => {
+  //   setEndTime(time);
+  //   const start = new Date(startTime);
+  //   const end = new Date(time);
+  //   const timeDiff = end - start;
+  //   const hoursDiff = Math.round(timeDiff / (1000 * 60 * 60));
+  //   setHours(hoursDiff);
+  //   setIsDateTimeSelected(true);
+  // };
+
+  // useEffect(() => {
+  //   setIsButtonEnabled(isDateTimeSelected);
+  // }, [isDateTimeSelected]);
+
+  // const handleReservation = () => {
+  //   const quantity = hours;
+  //   const date = new Date(selectedDate);
+  //   //fecha y hora de inicio reserva
+  //   const start = new Date(selectedDate);
+  //   start.setHours(startTime.getHours());
+  //   start.setMinutes(startTime.getMinutes());
+  //   //fecha y hora de finalizacion de reserav
+  //   const end = new Date(selectedDate);
+  //   end.setHours(endTime.getHours());
+  //   end.setMinutes(endTime.getMinutes());
+  //   //obj para enviar al carrito(x redux)
+  //   const reservation = {
+  //     idService: serviceDetail.id,
+  //     nameService: serviceDetail.nameService,
+  //     typeService: serviceDetail.typeService,
+  //     location: serviceDetail.location,
+  //     description: serviceDetail.description,
+  //     pricePerHour: serviceDetail.pricePerHour || null,
+  //     reservationData: {
+  //       date,
+  //       start,
+  //       end,
+  //       quantity,
+  //     },
+  //   };
+
+  //   console.log("Reservation", reservation);
+  //   console.log("Start Time:", start);
+  //   console.log("End Time:", end);
+  //   console.log("Hours:", hours);
+  //   console.log("queantity", quantity);
+  //   // notify();
+  // };
+ 
+
+    // <div className={style.reservationContainer}>
+    //     <div className={style.calendarContainer}>
+    //       <p className={style.selectDateLabel}>Select Date:</p>
+    //       <DatePicker
+    //         selected={selectedDate}
+    //         onChange={handleDateChange}
+    //         minDate={new Date()}
+    //         dateFormat="dd/MM/yyyy"
+    //         className={style.datePicker}
+    //         readOnlyInput
+    //       />
+    //     </div>
+    //     <div className={style.calendarContainer}>
+    //       <p className={style.selectTimeLabel}>Select Start Time:</p>
+    //       <DatePicker
+    //         selected={startTime}
+    //         onChange={handleStartTimeChange}
+    //         showTimeSelect
+    //         showTimeSelectOnly
+    //         timeIntervals={60}
+    //         timeCaption="Time"
+    //         dateFormat="HH:mm"
+    //         className={style.datePicker}
+    //         disabled={!selectedDate}
+    //         readOnlyInput
+    //       />
+    //     </div>
+    //     <div className={style.calendarContainer}>
+    //       <p className={style.selectTimeLabel}>Select End Time:</p>
+    //       <DatePicker
+    //         selected={endTime}
+    //         onChange={handleEndTimeChange}
+    //         showTimeSelect
+    //         showTimeSelectOnly
+    //         timeIntervals={60}
+    //         timeCaption="Time"
+    //         dateFormat="HH:mm"
+    //         className={style.datePicker}
+    //         disabled={!startTime}
+    //         minTime={
+    //           startTime
+    //             ? new Date(startTime.getTime() + 60 * 60 * 1000)
+    //             : new Date().setHours(23, 0)
+    //         }
+    //         maxTime={new Date().setHours(23, 0)}
+    //         readOnlyInput
+    //       />
+    //     </div>
+    //     <p>Hours Selected: {hours}</p>
+    //     <button
+    //       onClick={handleReservation}
+    //       className={`${style.reservationButton} ${
+    //         isButtonEnabled ? style.enabled : style.disabled
+    //       }`}
+    //       disabled={!isButtonEnabled}
+    //     >
+    //       Confirm Reservation
+    //     </button>
+    //   </div>
+    //   <Toaster
+    //     position="bottom-right"
+    //     toastOptions={{
+    //       className: "",
+    //       style: {
+    //         border: "1px solid #713200",
+    //         padding: "16px",
+    //         color: "#713200",
+    //       },
+    //     }}
+    //   /> 
