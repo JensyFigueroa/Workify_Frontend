3cb8af8f9d8b4c234d91f6e2ff9f372fd5fc2573
Carlitossaul <carlitossaul.h@gmail.com>
2023-06-01 22:42:29 -0300

    notificacion por email al usuario listo

--- src/components/views/SuccessPayment/SuccessPayment.jsx
+++ src/components/views/SuccessPayment/SuccessPayment.jsx
@@ -6,23 +6,16 @@ import { useEffect } from "react";
 
 const SuccessPayment = () => {
   const user = useSelector((state) => state.currentUserNameLoggedIn);
-  console.log(user);
-
-  const persona = { email: "carlitossaul.h@gmail.com" };
 
   useEffect(() => {
-    // Configura las credenciales de email.js
     emailjs.init("7W9NT4oCDCiTSPAhs");
 
-    // Configura el mensaje de correo electrónico
     const templateParams = {
-      to_email: persona.email, // Reemplaza "user.email" con la propiedad correcta del objeto user que contiene el correo electrónico del usuario
-      from_name: "Workify Services", // Nombre del remitente del correo electrónico
-      subject: "Pago exitoso", // Asunto del correo electrónico
-      message: `Hola ${user[0]},\n\nEl pago ha sido exitoso. Gracias por utilizar nuestros servicios.\n\nSaludos, \nWorkify Services`, // Cuerpo del correo electrónico
+      to_email: user[1],
+      from_name: "Workify Services",
+      subject: "Pago exitoso",
+      message: `Hola ${user[0]},\n\nEl pago ha sido exitoso. Hemos notificado a los prestadores de servicios, en las proximas horas los proveedores de servicios estaran siendo notificados y aceptando o declinando su oferta.\n\nSaludos, \nWorkify Services`,
     };
-
-    // Envía el correo electrónico
     emailjs
       .send("service_3iqa24w", "template_o4thiyt", templateParams)
       .then((response) => {
