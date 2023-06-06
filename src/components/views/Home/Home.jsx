69a5e11b5aef54014cfb9b647623d0ae763154aa
Carlitossaul <carlitossaul.h@gmail.com>
2023-06-06 11:30:13 -0300

    comenzando a modificar el carrito para usar la base de datos

--- src/components/views/Home/Home.jsx
+++ src/components/views/Home/Home.jsx
@@ -3,13 +3,17 @@ import { Cards } from "../../Cards/Cards";
 import FilterBar from "../../FilterBar/FilterBar";
 import styles from "./Home.module.css";
 import { Loading } from "../../Loading/Loading";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { all } from "axios";
 const Home = () => {
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
   //toco daniel linea 10 y 11
   const uidHome = useSelector((state) => state.currentUserIdLoggedIn);
   const nameHome = useSelector((state) => state.currentUserNameLoggedIn);
+
+  // if (uidHome) dispatch();
+
   console.log(uidHome, "uid desde home con use selector");
   console.log(nameHome, "name desde home con use selector");
   const allServices = useSelector((state) => state.allServices);
@@ -17,7 +21,7 @@ const Home = () => {
     setTimeout(() => setLoading(false), 4000);
     window.scrollTo(0, 0);
   }, []);
-  
+
   useEffect(() => {
     window.scrollTo(0, 0);
     if (allServices.length > 0) {
